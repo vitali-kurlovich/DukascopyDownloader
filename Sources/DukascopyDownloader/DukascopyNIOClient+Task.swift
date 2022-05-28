df441a7c376198df70daee8f1eaaee40e8070845
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-05-28 17:09:46 +0300

    Create DukascopyNIOClient+Task.swift

---
+++ Sources/DukascopyDownloader/DukascopyNIOClient+Task.swift
@@ -0,0 +1,68 @@
+//
+//  Created by Vitali Kurlovich on 28.05.22.
+//
+
+import AsyncHTTPClient
+import DukascopyURL
+import Foundation
+import NIO
+import NIOHTTP1
+
+private var urlFactory = DukascopyRemoteURL()
+
+public
+extension DukascopyNIOClient {
+    typealias Format = DukascopyRemoteURL.Format
+
+    struct FetchTask {
+        public let format: Format
+        public let filename: String
+        public let period: Range<Date>
+
+        public let result: EventLoopFuture<(data: ByteBuffer?, filename: String, period: Range<Date>)>
+    }
+
+    enum FetchTaskError: Error {
+        case requestFailed(HTTPResponseStatus)
+    }
+
+    func tasks(format: Format, for filename: String, range: Range<Date>) throws -> [FetchTask] {
+        let quotes = urlFactory.quotes(format: format, for: filename, range: range)
+
+        return try quotes.map { (url: URL, range: Range<Date>, _, _) -> FetchTask in
+            let request = try HTTPClient.Request(url: url)
+
+            let future = client.execute(request: request)
+
+            let result = future.flatMapThrowing { respose throws -> (data: ByteBuffer?, filename: String, period: Range<Date>) in
+                let status = respose.status
+                if respose.status != .ok {
+                    throw FetchTaskError.requestFailed(status)
+                }
+
+                return (data: respose.body, filename: filename, period: range)
+            }
+
+            return .init(format: format, filename: filename, period: range, result: result)
+        }
+    }
+
+    func task(format: Format, for filename: String, date: Date) throws -> FetchTask {
+        let quotes = urlFactory.quotes(format: format, for: filename, date: date)
+
+        let request = try HTTPClient.Request(url: quotes.url)
+
+        let future = client.execute(request: request)
+
+        let result = future.flatMapThrowing { respose throws -> (data: ByteBuffer?, filename: String, period: Range<Date>) in
+            let status = respose.status
+            if respose.status != .ok {
+                throw FetchTaskError.requestFailed(status)
+            }
+
+            return (data: respose.body, filename: filename, period: quotes.range)
+        }
+
+        return .init(format: format, filename: filename, period: quotes.range, result: result)
+    }
+}
