e481b9b964a1b6912e5e30039f273b1e350414fd
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-05-28 17:09:49 +0300

    Update DukascopyNIOClient.swift

--- Sources/DukascopyDownloader/DukascopyNIOClient.swift
+++ Sources/DukascopyDownloader/DukascopyNIOClient.swift
@@ -3,31 +3,14 @@
 //
 
 import AsyncHTTPClient
-import DukascopyURL
+
 import Foundation
 import Logging
 import NIO
-import NIOHTTP1
 
 public
 final class DukascopyNIOClient {
-    public typealias Format = DukascopyRemoteURL.Format
-
-    private let client: HTTPClient
-    private let urlFactory = DukascopyRemoteURL()
-
-    public init(eventLoopGroup: EventLoopGroup,
-                configuration: HTTPClient.Configuration = .init(),
-                backgroundActivityLogger: Logger)
-    {
-        client = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup), configuration: configuration, backgroundActivityLogger: backgroundActivityLogger)
-    }
-
-    public init(eventLoopGroup: EventLoopGroup,
-                configuration: HTTPClient.Configuration = .init())
-    {
-        client = HTTPClient(eventLoopGroupProvider: .shared(eventLoopGroup), configuration: configuration)
-    }
+    internal let client: HTTPClient
 
     public init(eventLoopGroupProvider: HTTPClient.EventLoopGroupProvider,
                 configuration: HTTPClient.Configuration = .init(),
@@ -49,55 +32,16 @@ final class DukascopyNIOClient {
 
 public
 extension DukascopyNIOClient {
-    struct FetchTask {
-        public let format: Format
-        public let filename: String
-        public let period: Range<Date>
-
-        public let result: EventLoopFuture<(data: ByteBuffer?, filename: String, period: Range<Date>)>
+    convenience init(eventLoopGroup: EventLoopGroup,
+                     configuration: HTTPClient.Configuration = .init(),
+                     backgroundActivityLogger: Logger)
+    {
+        self.init(eventLoopGroupProvider: .shared(eventLoopGroup), configuration: configuration, backgroundActivityLogger: backgroundActivityLogger)
     }
 
-    enum FetchTaskError: Error {
-        case requestFailed(HTTPResponseStatus)
-    }
-
-    func tasks(format: Format, for filename: String, range: Range<Date>) throws -> [FetchTask] {
-        let quotes = urlFactory.quotes(format: format, for: filename, range: range)
-
-        return try quotes.map { (url: URL, range: Range<Date>, _, _) -> FetchTask in
-            let request = try HTTPClient.Request(url: url)
-
-            let future = client.execute(request: request)
-
-            let result = future.flatMapThrowing { respose throws -> (data: ByteBuffer?, filename: String, period: Range<Date>) in
-                let status = respose.status
-                if respose.status != .ok {
-                    throw FetchTaskError.requestFailed(status)
-                }
-
-                return (data: respose.body, filename: filename, period: range)
-            }
-
-            return .init(format: format, filename: filename, period: range, result: result)
-        }
-    }
-
-    func task(format: Format, for filename: String, date: Date) throws -> FetchTask {
-        let quotes = urlFactory.quotes(format: format, for: filename, date: date)
-
-        let request = try HTTPClient.Request(url: quotes.url)
-
-        let future = client.execute(request: request)
-
-        let result = future.flatMapThrowing { respose throws -> (data: ByteBuffer?, filename: String, period: Range<Date>) in
-            let status = respose.status
-            if respose.status != .ok {
-                throw FetchTaskError.requestFailed(status)
-            }
-
-            return (data: respose.body, filename: filename, period: quotes.range)
-        }
-
-        return .init(format: format, filename: filename, period: quotes.range, result: result)
+    convenience init(eventLoopGroup: EventLoopGroup,
+                     configuration: HTTPClient.Configuration = .init())
+    {
+        self.init(eventLoopGroupProvider: .shared(eventLoopGroup), configuration: configuration)
     }
 }
