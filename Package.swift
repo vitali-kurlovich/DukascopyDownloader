3d706b0c01fe215a373e038cd322eaa984f0176d
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-05-31 01:00:30 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -19,9 +19,9 @@ let package = Package(
 
         .package(url: "https://github.com/vitali-kurlovich/DukascopyURL.git", from: "2.3.0"),
 
-        .package(url: "https://github.com/vitali-kurlovich/DukascopyModel.git", from: "1.9.0"),
+        .package(url: "https://github.com/vitali-kurlovich/DukascopyModel.git", from: "1.10.0"),
 
-        .package(url: "https://github.com/vitali-kurlovich/DukascopyDecoder.git", from: "2.4.0"),
+        .package(url: "https://github.com/vitali-kurlovich/DukascopyDecoder.git", from: "2.5.0"),
 
     ],
     targets: [
