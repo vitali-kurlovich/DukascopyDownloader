6065c26110e62455c2d19fa5274b1ccf00acbb0e
vitali-kurlovich <vitalikurlovich@gmail.com>
2022-06-09 12:30:01 +0300

    Update DukascopyNIOClientTests.swift

--- Tests/DukascopyDownloaderTests/DukascopyNIOClientTests.swift
+++ Tests/DukascopyDownloaderTests/DukascopyNIOClientTests.swift
@@ -172,25 +172,11 @@ class DukascopyNIOClientTests: XCTestCase {
     }
 
     func testFetchQuoteTicks() throws {
-        let expectation = XCTestExpectation(description: "Download ticks")
+        let expectation = XCTestExpectation(description: "Fetch ticks")
 
         let downloader = DukascopyNIOClient(eventLoopGroupProvider: .createNew)
 
-        let groups = downloader.fetchInstruments()
-
-        let instrument = groups.flatMapThrowing { groups -> Instrument in
-            let root = Group(id: "", title: "", groups: groups)
-
-            let instrument = root.first { instrument in
-                instrument.symbol == "USD/THB"
-            }
-
-            guard let thb = instrument else {
-                throw TestError.doNotFindInstrument
-            }
-
-            return thb
-        }
+        let instrument = downloader.fetchInstrument(by: "USD/THB", caseInsensitive: false)
 
         let begin = formatter.date(from: "02-01-2020 01:00")!
 
@@ -229,28 +215,49 @@ class DukascopyNIOClientTests: XCTestCase {
         }
 
         wait(for: [expectation], timeout: 10.0)
+
+        let expSymbol = XCTestExpectation(description: "Fetch ticks by symbol")
+
+        let result_1 = downloader.fetchQuoteTicks(by: "usd/thb", date: begin)
+
+        result_1.whenSuccess { (instrument: Instrument, _: Range<Date>, ticks: [Tick]) in
+            XCTAssertEqual(instrument.symbol, "USD/THB")
+
+            XCTAssertEqual(ticks.count, 1096)
+
+            // 02.01.2020 04:00:00.138 GMT+0300,30.1296,30.113400000000002,1,1
+            let firstTick = ticks.first!
+            XCTAssertEqual(firstTick.time, 138)
+            XCTAssertEqual(firstTick.askp, 301_296)
+            XCTAssertEqual(firstTick.bidp, 301_134)
+            XCTAssertEqual(firstTick.askv, 1)
+            XCTAssertEqual(firstTick.bidv, 1)
+
+            // 02.01.2020 04:59:53.390 GMT+0300,30.1186,30.107400000000002,1.1,1.1
+            let lastTick = ticks.last!
+
+            XCTAssertEqual(lastTick.time, (59 * 60 + 53) * 1000 + 390)
+            XCTAssertEqual(lastTick.askp, 301_186)
+            XCTAssertEqual(lastTick.bidp, 301_074)
+            XCTAssertEqual(lastTick.askv, 1.1)
+            XCTAssertEqual(lastTick.bidv, 1.1)
+
+            expSymbol.fulfill()
+        }
+
+        result.whenFailure { error in
+            XCTFail(error.localizedDescription)
+        }
+
+        wait(for: [expSymbol], timeout: 10.0)
     }
 
     func testFetchQuoteTicks_1() throws {
-        let expectation = XCTestExpectation(description: "Download ticks")
+        let expectation = XCTestExpectation(description: "Fetch ticks")
 
         let downloader = DukascopyNIOClient(eventLoopGroupProvider: .createNew)
 
-        let groups = downloader.fetchInstruments()
-
-        let instrument = groups.flatMapThrowing { groups -> Instrument in
-            let root = Group(id: "", title: "", groups: groups)
-
-            let instrument = root.first { instrument in
-                instrument.symbol == "USD/THB"
-            }
-
-            guard let thb = instrument else {
-                throw TestError.doNotFindInstrument
-            }
-
-            return thb
-        }
+        let instrument = downloader.fetchInstrument(by: "Usd/Thb")
 
         let begin = formatter.date(from: "02-01-2020 01:00")!
         let end = formatter.date(from: "02-01-2020 03:00")!
@@ -289,6 +296,40 @@ class DukascopyNIOClientTests: XCTestCase {
         }
 
         wait(for: [expectation], timeout: 10.0)
+
+        let expSymbol = XCTestExpectation(description: "Fetch ticks by symbol")
+
+        let result_1 = downloader.fetchQuoteTicks(by: "usd/thb", range: begin ..< end)
+
+        result_1.whenSuccess { (instrument: Instrument, _: Range<Date>, ticks: [Tick]) in
+            XCTAssertEqual(instrument.symbol, "USD/THB")
+
+            XCTAssertEqual(ticks.count, 1096 + 695)
+
+            // 02.01.2020 04:00:00.138 GMT+0300,30.1296,30.113400000000002,1,1
+            let firstTick = ticks.first!
+            XCTAssertEqual(firstTick.time, 138)
+            XCTAssertEqual(firstTick.askp, 301_296)
+            XCTAssertEqual(firstTick.bidp, 301_134)
+            XCTAssertEqual(firstTick.askv, 1)
+            XCTAssertEqual(firstTick.bidv, 1)
+
+            let lastTick = ticks.last!
+
+            XCTAssertEqual(lastTick.time, (60 * 60) * 1000 + 3_599_269)
+            XCTAssertEqual(lastTick.askp, 301_116)
+            XCTAssertEqual(lastTick.bidp, 301_004)
+            XCTAssertEqual(lastTick.askv, 1)
+            XCTAssertEqual(lastTick.bidv, 1)
+
+            expSymbol.fulfill()
+        }
+
+        result.whenFailure { error in
+            XCTFail(error.localizedDescription)
+        }
+
+        wait(for: [expSymbol], timeout: 10.0)
     }
 }
 
